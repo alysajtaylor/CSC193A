77e84b32cfb17a68f0d0e2302171e02c2f17fc02
jennifer_taylor <alysajtaylor@outlook.com>
2026-03-22 20:45:50 -0700

    fancy= random rgb + italic, Boring = normal black

--- Assignment 5/fancifymytext.js
+++ Assignment 5/fancifymytext.js
@@ -1,7 +1,22 @@
  function bigClick() {
-    alert("Hello, World!");
+    document.getElementById("text").style.fontSize = "24pt";
 }
 
+function fancifyClick() {
+    let red = Math.floor(Math.random() * (255 - 1)) + 1;
+    let green = Math.floor(Math.random() * (255 - 1)) + 1;
+    let blue = Math.floor(Math.random() * (255 - 1))+ 1;
+
+    document.getElementById("text").style.fontStyle = "italic";
+    document.getElementById("text").style.color = "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+function boringClick() {
+    document.getElementById("text").style.fontStyle = "normal";
+    document.getElementById("text").style.color = "black";
+}
+
+// event listeners 
 document.getElementById("bigger").addEventListener("click", bigClick);
-
-
+document.getElementById("fancy").addEventListener("click", fancifyClick);
+document.getElementById("boring").addEventListener("click", boringClick);
