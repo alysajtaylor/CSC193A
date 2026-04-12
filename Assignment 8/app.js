a3a82b20ad11b1a8e2b08dbe744ee72bb2501344
jennifer_taylor <alysajtaylor@outlook.com>
2026-04-11 17:09:25 -0700

    completes both exercises

--- Assignment 8/app.js
+++ Assignment 8/app.js
@@ -8,9 +8,26 @@ app.get('/math/circle/:r', function(req, res) {
     const r = parseFloat(req.params.r);
     const area = Math.PI * r * r;
     const circumference = 2 * Math.PI * r;
-    res.json({ "area": area, "circumference": circumference });
+    res.json({ "area": parseFloat(area.toFixed(2)), "circumference": parseFloat(circumference.toFixed(2)) });
+});
 
 // define endpoint for exercise 2 here
+app.get('/hello/name', function(req, res) {
+    const fname = req.query.first;
+    const lname = req.query.last;
+    if (!fname && !lname) {
+        res.status(400).send("Missing Required GET parameters: first, last");
+    }
+    else if (!fname) {
+        res.status(400).send("Missing Required GET parameter: first");
+    }
+    else if (!lname) {
+        res.status(400).send("Missing Required GET parameter: last");
+    }
+    else {
+        //res.type("text");
+        res.send(`Hello, ${fname} ${lname}`);
+    }
 });
 
 const PORT = process.env.PORT || 8000;
